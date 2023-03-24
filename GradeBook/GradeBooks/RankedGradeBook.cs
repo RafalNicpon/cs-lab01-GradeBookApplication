21b6b2e4897207661e8f9cd21eb2fb06aa7810b9
MrHax <rafal.nicpon1@gmail.com>
2023-03-24 23:52:20 +0100

    Override RankedGradeBook's GetLetterGrade method

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -12,5 +12,38 @@ namespace GradeBook.GradeBooks
         {
             Type = Enums.GradeBookType.Ranked;
         }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count < 5)
+            {
+                throw new InvalidOperationException("Podaj minimum 5 studentów");
+            }
+
+            var betterStudents = Students.Select(w => w.AverageGrade > averageGrade).Count();
+
+            if (betterStudents/Students.Count <= 0.2f)
+            {
+                return 'A';
+            }
+            else if (betterStudents / Students.Count <= 0.4f)
+            {
+                return 'B';
+            }
+            else if (betterStudents / Students.Count <= 0.6f)
+            {
+                return 'C';
+            }
+            else if (betterStudents / Students.Count <= 0.8f)
+            {
+                return 'D';
+            }
+            else
+            {
+                return 'F';
+            }
+        }
+       
+
     }
 }
